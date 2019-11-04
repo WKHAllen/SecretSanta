c77b861bb500b1dd6b64a44f89f79ce7deca31d9
WKHAllen <wkhallen@gmail.com>
2019-11-04 10:59:25 -0600

    Improve front-end

--- src/static/main.js
+++ src/static/main.js
@@ -1,24 +1,40 @@
 var rows = 0;
+const idLength = 8;
 
 const formRow = `
-    <div class="form-row" id="form-main-row-{}">
+    <div class="form-row" id="form-main-row-{0}">
         <div class="form-group col-5">
-            <input type="text" class="form-control" name="name-{}" placeholder="Name" autocomplete="off" />
+            <input type="text" class="form-control" name="name-{0}" placeholder="Name" autocomplete="off" />
         </div>
         <div class="form-group col">
-            <input type="email" class="form-control" name="email-{}" placeholder="Email" autocomplete="off" />
+            <input type="email" class="form-control" name="email-{0}" placeholder="Email" autocomplete="off" />
         </div>
         <div class="form-group col-auto">
-            <button type="button" class="btn btn-light btn-block" onclick="removeRow({});">&times;</button>
+            <button type="button" class="btn btn-light btn-block" onclick="removeRow({0});">&times;</button>
         </div>
     </div>
 `;
 
+// This will become useful in the unlikely event that Google decides to fix their Chrome autocomplete problem
+function randomId(length) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++)
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    return result;
+}
+
+function replaceAll(string, ...functions) {
+    for (var i = 0; i < functions.length; i++)
+        while (string.includes(`{${i}}`))
+            string = string.replace(`{${i}}`, functions[i]());
+    return string;
+}
+
 function addRow() {
-    var newRow = ++rows;
-    var newFormRow = formRow;
-    while (newFormRow.includes('{}'))
-        newFormRow = newFormRow.replace('{}', newRow);
+    ++rows;
+    var newFormRow = replaceAll(formRow, () => rows, () => randomId(idLength));
     document.getElementById('form-main').innerHTML += newFormRow;
 }
 
