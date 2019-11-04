12cd5c9954e66d7cc2ad1e43cce054a6600ca7c5
WKHAllen <wkhallen@gmail.com>
2019-11-03 19:30:08 -0600

    Use handlebars

--- src/index.js
+++ src/index.js
@@ -1,20 +1,40 @@
 const express = require('express');
+const hbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 const path = require('path');
 
-const publicDir = 'public';
-const errorDir = path.join(__dirname, publicDir, 'errors');
 const port = process.env.PORT || 3000;
+const staticPath = path.join(__dirname, 'static');
 
 var app = express();
 
-app.use(express.static(path.join(__dirname, publicDir)));
+app.engine('.html', hbs({
+    extname: '.html',
+    defaultView: 'default',
+    defaultLayout: 'default'
+}));
+app.set('view engine', '.html');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(staticPath));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.post('/', (req, res) => {
+    // TODO: application logic
+    res.render('success');
+});
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(errorDir, '404.html'));
+    res.status(404).render('404');
 });
 
 app.use((err, req, res, next) => {
-    res.status(500).sendFile(path.join(errorDir, '500.html'));
+    res.status(500).render('500');
+    if (err) console.log(err);
 });
 
 app.listen(port, () => {
