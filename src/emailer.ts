0303cf8e228d180f077f674f2dcd796e1364a743
WKHAllen <wkhallen@gmail.com>
2020-11-19 21:49:57 -0600

    Improve email sending

--- src/emailer.ts
+++ src/emailer.ts
@@ -3,43 +3,42 @@ import * as nodemailer from 'nodemailer';
 export const emailAddress = (process.env.EMAIL_ADDRESS as string);
 const emailPassword = process.env.EMAIL_APP_PASSWORD;
 
-export function sendEmail(emailTo: string, subject: string, html: string, text?: string) {
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        auth: {
-            user: emailAddress,
-            pass: emailPassword
-        }
+async function wait(ms: number): Promise<void> {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
     });
-    const mailOptions = {
-        from: {
-            name: 'Secret Santa',
-            address: emailAddress
-        },
-        to: emailTo,
-        subject: subject,
-        html: html,
-        text: text
-    };
-    transporter.sendMail(mailOptions, (err, info) => {
-        // In case gsmtp servers encounter problems (they have before)
-        if (err) {
-            console.warn('Error sending emails (try 1):', err);
-            setTimeout(() => {
-                transporter.sendMail(mailOptions, (err, info) => {
-                    if (err) {
-                        console.warn('Error sending emails (try 2):', err);
-                        setTimeout(() => {
-                            transporter.sendMail(mailOptions, (err, info) => {
-                                if (err) {
-                                    console.warn('Error sending emails (try 3):');
-                                    throw err;
-                                }
-                            });
-                        }, 10 * 60 * 1000);
-                    }
-                });
-            }, 60 * 1000);
-        }
+}
+
+export async function sendEmail(emailTo: string, subject: string, html: string, text?: string, tryNum: number = 1): Promise<void> {
+    return new Promise((resolve) => {
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            auth: {
+                user: emailAddress,
+                pass: emailPassword
+            }
+        });
+        const mailOptions = {
+            from: {
+                name: 'Secret Santa',
+                address: emailAddress
+            },
+            to: emailTo,
+            subject: subject,
+            html: html,
+            text: text
+        };
+        transporter.sendMail(mailOptions, (err, info) => {
+            if (err) {
+                console.warn(`Error sending email to '${emailTo}' (try ${tryNum}):\n`, err);
+                wait(60 * 1000)
+                    .then(() => {
+                        sendEmail(emailTo, subject, html, text, tryNum + 1)
+                            .then(resolve);
+                    });
+            } else {
+                resolve();
+            }
+        });
     });
 }
